198a3bf1856b14580c7cfa2a4279256923aee2b6
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2021-05-21 14:39:10 +0200

    Added some help text for server

--- 04-from-scratch/hello.go
+++ 04-from-scratch/hello.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("Welcome, user! Curl me with your name as path :-D")
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("Welcome, user! Curl me (locally with Port 8080) with your name as path :-D")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
